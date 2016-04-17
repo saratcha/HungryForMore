2cb743c90d2b8199b272baf0442106bf312638dd
sarat <sarat.ogirala@gmail.com>
2016-04-17 12:09:57 -0700

    Testing

---
+++ src/hungryForMore/BinarySearchTree.java
@@ -0,0 +1,17 @@
+package hungryForMore;
+
+public class BinarySearchTree 
+{
+	BinarySearchTree()
+	{
+		System.out.println("TestingBinarySearchTree");
+	}
+
+	/*public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Testing BinarySearchTree");
+
+	}*/
+
+}
---
+++ src/hungryForMore/HungryForMore.java
@@ -0,0 +1,14 @@
+package hungryForMore;
+
+public class HungryForMore
+{
+
+	public static void main(String[] args)
+	{
+		/*BinarySearchTree bst = */ new BinarySearchTree();
+		// TODO Auto-generated method stub
+		System.out.println("Testing HungryForMore");
+
+	}
+
+}
